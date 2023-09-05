9cd92a8f25050a8eeaf365ad43944a16e5aa1d83
BrandonPonce MBP <p6nce@outlook.com>
2023-09-05 02:03:05 -0400

    front end complete

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-delete');
+const previousOperandTextElement = document.querySelector('[data-previous-operand');
+const currentOperandTextElement = document.querySelector('[data-current-operand');
+
+
